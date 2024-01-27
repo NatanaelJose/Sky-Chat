ff5a10d0861aafbc77acd20fab321352cff0b1b9
NatanaelJose <josesaraivamaciel@gmail.com>
2024-01-27 15:52:30 -0300

    init

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+
+const MyComponent = () => {
+  return (
+    <div className="bg-blue-500 text-white p-4">
+      Este é um componente React com estilos do Tailwind CSS!
+    </div>
+  );
+};
+
+export default MyComponent;
