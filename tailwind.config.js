dd2e3874d1c9f7f090aecd2f6ef43b4c63c5b676
NatanaelJose <josesaraivamaciel@gmail.com>
2024-02-03 18:18:41 -0300

    feat: icon

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,8 +3,6 @@ export default {
   content: [
     "./src/**/*.{js,ts,jsx,tsx}",],
     theme: {
-      ...require("tailwindcss/colors"),
-      primary:"purple",
     extend: {
       fontFamily: {
         sans: ['Segoe UI', 'sans-serif'],
